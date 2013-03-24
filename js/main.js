bb8f5080eb026dfab5e8bdf0f89e0e140a6f16d4
Defmoves <defmoves@gmail.com>
2013-03-24 14:38:22 +0000

    JS update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+(function(){
 
+	$(document).ready(function(){
+
+		// Camera
+		$('.camera_wrap').camera({
+			onEndTransition: function() {
+				var ind = $('.camera_target .cameraSlide.cameracurrent').index();
+				console.log(ind);
+			}
+		});
+
+
+	});
+
+
+}());
