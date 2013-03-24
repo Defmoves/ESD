55c035dd8b021b44dfc3896d7c298bf4ff44ca7a
Defmoves <defmoves@gmail.com>
2013-03-24 15:59:37 +0000

    full push

--- js/main.js
+++ js/main.js
@@ -3,17 +3,13 @@
 	$(document).ready(function(){
 
 		// Camera
-		/*
 		$('.camera_wrap').camera({
-			onEndTransition: function() {
+			onStartLoading: function() {
 				var ind = $('.camera_target .cameraSlide.cameracurrent').index();
 				console.log(ind);
+				$("#intro").typewriter();
 			}
 		});
-		*/
-
-		// jticker
-		$("#intro").typewriter();
 
 	});
 
